962177a2c6f4374724a91eff1b09cf33afeabc28
cahito <cahito@gmail.com>
2022-07-13 18:28:26 -0300

    Feat - Requisito um cumprido

---
+++ src/database/migrations/20220713194303-create-postcategory.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
