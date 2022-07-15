70d68a9be00164e7cd25722c133691826ef97abc
cahito <cahito@gmail.com>
2022-07-15 15:12:14 -0300

    Feat - Requisito sete cumprido

---
+++ src/database/models/category.js
@@ -0,0 +1,13 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: DataTypes.STRING,
+  }, { timestamps: false });
+
+  return Category;
+};
+
+module.exports = Category;
